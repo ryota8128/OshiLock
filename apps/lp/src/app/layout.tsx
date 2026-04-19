75e63f3d6e60a956be0533fd901ebdcc52c02fc0
ryota8128 <ryota2339@icloud.com>
2026-04-20 01:22:41 +0900

    feat: Google Tag Manager追加

--- apps/lp/src/app/layout.tsx
+++ apps/lp/src/app/layout.tsx
@@ -107,6 +107,16 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KVXCSQR4');`,
+          }}
+        />
       </head>
       <body className="bg-paper text-ink font-sans">{children}</body>
     </html>
